7f3446be5630bd9eb01824be3738449a72b439db
Keval Juthani <keval.j@gmail.com>
2024-07-07 01:44:33 +0530

    CORS Updated

--- config/middlewares.js
+++ config/middlewares.js
@@ -1,9 +1,18 @@
+// config/middlewares.js
 module.exports = [
-  'strapi::logger',
   'strapi::errors',
   'strapi::security',
   'strapi::cors',
+  {
+    name: 'strapi::cors',
+    config: {
+      origin: ['http://localhost:3000','https://ayna-frontend.netlify.app'],
+      methods: ['GET', 'POST', 'PUT', 'PATCH', 'DELETE', 'OPTIONS'],
+      headers: ['Content-Type', 'Authorization'],
+    },
+  },
   'strapi::poweredBy',
+  'strapi::logger',
   'strapi::query',
   'strapi::body',
   'strapi::session',
